3402c411dbdda501777e1e7f81be2d6c680bef5c
项升 <xiangsheng.gh@alibaba-inc.com>
2019-05-05 20:14:30 +0800

    Init

---
+++ src/main/java/com/aliware/GatewayApp.java
@@ -0,0 +1,10 @@
+package com.aliware;
+
+/**
+ * @author guohaoice@gmail.com
+ */
+public class GatewayApp {
+  public static void main(String[] args) {
+    //
+  }
+}
---
+++ src/main/java/com/aliware/ProviderApp.java
@@ -0,0 +1,10 @@
+package com.aliware;
+
+/**
+ * @author guohaoice@gmail.com
+ */
+public class ProviderApp {
+  public static void main(String[] args) {
+    //
+  }
+}
