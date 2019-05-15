50f76a764cf0fab7ea694afddcc9f7440bdd296e
xujingfeng <jingfeng.xjf@alibaba-inc.com>
2019-05-15 17:30:40 +0800

    add serverFilter demo & add local benchmark statement to readme & add faq doc

---
+++ workspace-provider/src/main/java/com/aliware/tianchi/TestServerFilter.java
@@ -0,0 +1,30 @@
+package com.aliware.tianchi;
+
+import org.apache.dubbo.common.Constants;
+import org.apache.dubbo.common.extension.Activate;
+import org.apache.dubbo.rpc.Filter;
+import org.apache.dubbo.rpc.Invocation;
+import org.apache.dubbo.rpc.Invoker;
+import org.apache.dubbo.rpc.Result;
+import org.apache.dubbo.rpc.RpcException;
+
+/**
+ * @author daofeng.xjf
+ *
+ * 服务端过滤器
+ * 可选接口
+ * 用户可以在服务端拦截请求和响应,捕获 rpc 调用时产生、服务端返回的已知异常。
+ */
+@Activate(group = Constants.PROVIDER)
+public class TestServerFilter implements Filter {
+    @Override
+    public Result invoke(Invoker<?> invoker, Invocation invocation) throws RpcException {
+        try{
+            Result result = invoker.invoke(invocation);
+            return result;
+        }catch (Exception e){
+            throw e;
+        }
+
+    }
+}
