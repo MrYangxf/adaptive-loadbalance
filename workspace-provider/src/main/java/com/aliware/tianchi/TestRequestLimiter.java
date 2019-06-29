e7043fec13de3477d2297b3d4d84ab34f3eadb3c
yangxf <xuefengyang@126.com>
2019-06-29 11:53:35 +0800

    test

--- workspace-provider/src/main/java/com/aliware/tianchi/TestRequestLimiter.java
+++ workspace-provider/src/main/java/com/aliware/tianchi/TestRequestLimiter.java
@@ -2,14 +2,11 @@ package com.aliware.tianchi;
 
 import com.aliware.tianchi.common.metric.InstanceStats;
 import com.aliware.tianchi.common.util.RuntimeInfo;
-import com.aliware.tianchi.common.util.SegmentCounter;
-import com.aliware.tianchi.common.util.SkipListCounter;
 import com.aliware.tianchi.util.NearRuntimeHelper;
 import org.apache.dubbo.remoting.exchange.Request;
 import org.apache.dubbo.remoting.transport.RequestLimiter;
 
 import java.util.concurrent.ThreadLocalRandom;
-import java.util.concurrent.TimeUnit;
 
 import static com.aliware.tianchi.common.util.ObjectUtil.nonNull;
 
@@ -25,9 +22,6 @@ public class TestRequestLimiter implements RequestLimiter {
     private static final double THRESHOLD = .95d;
     private final NearRuntimeHelper helper = NearRuntimeHelper.INSTANCE;
 
-    private SegmentCounter counter = new SkipListCounter();
-    private SegmentCounter activeCounter = new SkipListCounter();
-    
     /**
      * @param request         服务请求
      * @param activeTaskCount 服务端对应线程池的活跃线程数
@@ -36,12 +30,8 @@ public class TestRequestLimiter implements RequestLimiter {
      */
     @Override
     public boolean tryAcquire(Request request, int activeTaskCount) {
-        long offset = getOffset();
-        counter.increment(offset);
-        activeCounter.add(offset, activeTaskCount);
         InstanceStats stats = helper.getInstanceStats();
         if (nonNull(stats)) {
-            stats.setActiveCount((int) (activeCounter.get(offset) / counter.get(offset)));
             RuntimeInfo runtimeInfo = helper.getRuntimeInfo();
             if (nonNull(runtimeInfo)) {
                 double processCpuLoad = runtimeInfo.getProcessCpuLoad();
@@ -54,10 +44,6 @@ public class TestRequestLimiter implements RequestLimiter {
             return activeTaskCount < stats.getDomainThreads();
         }
         return true;
-    }
-    
-    private long getOffset() {
-        return TimeUnit.MILLISECONDS.toSeconds(System.currentTimeMillis());
     }
 
 }
