70246f17731e244bc4744f5fa162ef38d3345ecf
yangxf <xuefengyang@126.com>
2019-07-13 10:28:22 +0800

    adjust

--- workspace-common/src/main/java/com/aliware/tianchi/common/conf/Configuration.java
+++ workspace-common/src/main/java/com/aliware/tianchi/common/conf/Configuration.java
@@ -35,7 +35,7 @@ public class Configuration implements Serializable {
     /**
      * 指标统计时间窗口配置
      */
-    private long windowSizeOfStats = 4;
+    private long windowSizeOfStats = 2;
 
     private long timeIntervalOfStats = 50;
 
